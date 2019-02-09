097d385722c4465b1916670445ebc4d3084179bc
Kearnie Lin <kearnie@Kearnies-MBP.wv.cc.cmu.edu>
2019-02-09 03:13:56 -0500

    pop up wip

--- content.js
+++ content.js
@@ -3,11 +3,12 @@ console.log("Chrome extension gogogo");
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-	console.log(message.txt);
+	console.log(message);
 	if (message.txt === "on") {
 		let paragraphs = document.getElementsByTagName('p');
 		for (elt of paragraphs) {
 			elt.style['font-size'] = '30px';
+			elt.style['line-height'] = '1.5';
 		}
 	}
 }
---
+++ popup.js
@@ -0,0 +1,44 @@
+// let userinput = document.getElementById('#userinput');
+// userinput.input(changeText);
+
+// function changeText() {
+// 	let params = {
+// 		active: true,
+// 		currentWindow: true
+// 	};
+
+// 	chrome.tabs.query(params, gotTabs);
+// 	function gotTabs(tab) {
+// 		let message = userinput.value();
+// 		console.log(message);
+// 		let msg = {
+// 			txt: "hello"
+// 		};
+// 		chrome.tabs.sendMessage(tab[0].id, msg);
+// 	}
+// }
+
+let zoom = document.getElementById('zoomText');
+
+zoom.onclick = zoomThatText;
+
+function zoomThatText() {
+	let paragraphs = document.getElementsByTagName('p');
+	for (elt of paragraphs) {
+		elt.style['font-size'] = '30px';
+		elt.style['line-height'] = '1.5';
+	}
+}
+
+//chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+	console.log(message);
+	if (message.txt === "on") {
+		let paragraphs = document.getElementsByTagName('p');
+		for (elt of paragraphs) {
+			elt.style['font-size'] = '30px';
+			elt.style['line-height'] = '1.5';
+		}
+	}
+}
